951f3957e95295a3c54a6d8196371514ecc74536
junick25 <jorsie25@outlook.com>
2025-06-16 22:12:07 -0500

    agregamos los arvchivos de entities

--- src/main/java/com/primerMicroSer/companies_crud/entities/webSite.java
+++ src/main/java/com/primerMicroSer/companies_crud/entities/webSite.java
@@ -1,23 +1,6 @@
 package com.primerMicroSer.companies_crud.entities;
 
 
-/*
-mapeo de bases de datos
-    traducir estructuras de objetos en tu código a tablas de una base de datos, y viceversa.
 
-ejemplo sencillo:
-tenemos esta clase java
-public class Company {
-    private Long id;       // -> Columna "id" (PK)
-    private String name;   // -> Columna "name"
-}
-por ende en la base de datos se debe expresar como
-CREATE TABLE company (
-    id   BIGINT PRIMARY KEY,
-    name VARCHAR(255)
-);
-
-Hibernate nos permite que las clases representen tablas
-*/
 public class webSite {
 }
