4c47665c4a682ccedad36de5e79a1038559e89c0
jorge <jorgesierralaiton@gmail.com>
2025-06-25 18:05:17 -0500

    pequenio avance

--- src/main/java/com/primerMicroSer/companies_crud/services/CompanyServiceImplementation.java
+++ src/main/java/com/primerMicroSer/companies_crud/services/CompanyServiceImplementation.java
@@ -2,11 +2,13 @@ package com.primerMicroSer.companies_crud.services;
 
 import com.primerMicroSer.companies_crud.entities.Company;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 
 @Service
 @Transactional
+@Slf4j
 public class CompanyServiceImplementation implements CompanyService {
 
 
